4dbed96509b89804d7cbce9e8121c0f00ff3c1da
zhaoyue111-zz <891444383@qq.com>
2026-07-30 10:40:53 +0800

    stage b v3训练结果

--- train_dhga_minimal.sh
+++ train_dhga_minimal.sh
@@ -12,8 +12,12 @@ CUDA_VISIBLE_DEVICES=0 conda run -n voxtell python run_3d_dhga.py \
   --dhga_semantic_adapter_rank 8 \
   --dhga_appearance_feature_layers -1 -2 -3 \
   --dhga_anchor_weight 0.25 \
+  --dhga_appearance_anchor_weight 0.25 \
+  --dhga_appearance_expansion_weight 0.1 \
   --dhga_cross_supervision_weight 1.0 \
+  --dhga_cross_supervision_min_weight 0.05 \
   --dhga_weak_strong_weight 0.5 \
+  --dhga_router_target_weight 0.5 \
   --dhga_prompt_ranking_weight 0 \
   --save_dir .save/dhga/stage_b \
   --sequences P0 \
